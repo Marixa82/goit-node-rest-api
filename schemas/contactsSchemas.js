224213b1287f7c563650fe12d6c91061c9991410
Marixa82 <121823496+Marixa82@users.noreply.github.com>
2024-01-21 20:18:38 +0200

    hw02-express

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -1,9 +1,13 @@
 import Joi from "joi";
 
 export const createContactSchema = Joi.object({
-
+    name: Joi.string().required().messages({ "any.required": "missing required name field" }),
+    email: Joi.string().required().messages({ "any.required": "missing required name field" }),
+    phone: Joi.string().required().messages({ "any.required": "missing required name field" }),
 })
 
 export const updateContactSchema = Joi.object({
-
+    name: Joi.string().messages({ "any.required": "Body must have at least one field" }),
+    email: Joi.string().messages({ "any.required": "Body must have at least one field" }),
+    phone: Joi.string().messages({ "any.required": "Body must have at least one field" }),
 })
