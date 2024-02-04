e9e939193bb78324e90759689218a0d3ef063856
Marixa82 <121823496+Marixa82@users.noreply.github.com>
2024-02-04 17:47:55 +0200

    upd

--- helpers/resizeAvatar.js
+++ helpers/resizeAvatar.js
@@ -1,19 +1,5 @@
 import Jimp from "jimp";
 
-// const url = "https://upload.wikimedia.org/wikipedia/commons/0/01/Bot-Test.jpg";
-
-// Jimp.read(url)
-//     .then((image) => {
-//         image.resize(250, 250)
-//     })
-//     .catch((error) => {
-//         console.error(error);
-//     });
-
-// function onBuffer(err, buffer) {
-//     if (err) throw err;
-//     console.log(buffer);
-// }
 const resizeAvatar = async (req, res, next) => {
     if (!req.file) {
         return next();
